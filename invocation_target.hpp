63bf9b0a45601a178b4a82df1b8a0a772ead3536
Matthew Milano <milano@cs.cornell.edu>
2019-12-11 16:45:53 -0500

    woo

--- invocation_target.hpp
+++ invocation_target.hpp
@@ -1,29 +1,50 @@
 #pragma once
 #include "named_member_fun.hpp"
+#include <memory>
 #include <tuple>
 #include <type_traits>
 
 #define TOF(arg) std::decay_t<decltype(arg)>
 
-template <typename> struct SingleRPCReceiver;
-template <int id, typename MemberFun> struct SingleRPCReceiver<named_member_fun<id, MemberFun>> {
-  SingleRPCReceiver(MemberFun) {}
-};
+template <typename Deserializer, typename Serializer, typename Bytes> struct RPCFramework {
+  struct GenericSingleRPCReceiver {
+    virtual Bytes invoke(const Deserializer &, const Serializer &, Bytes &) = 0;
+    virtual ~GenericSingleRPCReceiver() {}
+  };
 
-template <typename Class, typename... NamedMethods> class RPCReceiver : public SingleRPCReceiver<NamedMethods>... {
-public:
-  RPCReceiver(typename NamedMethods::fun_t... f) : SingleRPCReceiver<NamedMethods>(f)... {}
-};
+  template <typename> struct SingleRPCReceiver;
+  template <int id, typename MemberFun>
+  struct SingleRPCReceiver<named_member_fun<id, MemberFun>> : public GenericSingleRPCReceiver {
+    SingleRPCReceiver(MemberFun) {}
+    Bytes invoke(const Deserializer &, const Serializer &, Bytes &) override { return nullptr; }
+  };
 
-template <typename Class, typename... Methods> RPCReceiver<Class, Methods...> make_rpc_receiver(Methods... m) {
-  return RPCReceiver<Class, Methods...>{m.f...};
-}
+  template <typename Class, typename... NamedMethods> class RPCReceiver {
+    std::unique_ptr<Class> _this;
 
-template <typename T> class RPCReceiverBuilder {
+    std::array<std::unique_ptr<GenericSingleRPCReceiver>, sizeof...(NamedMethods)> invocation_targets;
 
-public:
-  template <typename... functions>
-  auto build_receiver(const functions &... f) -> RPCReceiver<T, TOF(ensure_named_member_fun(f))...> {
-    return make_rpc_receiver<T>(f...);
+  public:
+    RPCReceiver(Class *c, typename NamedMethods::fun_t... f)
+        : _this(c), invocation_targets{
+                             std::unique_ptr<GenericSingleRPCReceiver>{new SingleRPCReceiver<NamedMethods>(f)} ...} {}
+
+    Bytes invoke(const Deserializer &deserializer, const Serializer &serializer, Bytes &serialized, int target) {
+      return invocation_targets[target].invoke(deserializer, serializer, serialized);
+    }
+  };
+
+  template <typename Class, typename... Methods>
+  static RPCReceiver<Class, Methods...> make_rpc_receiver(Class *c, Methods... m) {
+    return RPCReceiver<Class, Methods...>{c, m.f...};
   }
+
+  template <typename T> class RPCReceiverBuilder {
+
+  public:
+    template <typename... functions>
+    auto build_receiver(T *t, const functions &... f) -> RPCReceiver<T, TOF(ensure_named_member_fun(f))...> {
+      return make_rpc_receiver(t, f...);
+    }
+  };
 };
--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include "invocation_target.hpp"
 
+struct deserializer_t {
+  template <typename T> static T *deserialize(char *) { return nullptr; }
+};
+struct serializer_t {
+  static char *serialize(...) { return nullptr; }
+};
+using bytes = char *;
+using simpleRPC = RPCFramework<deserializer_t, serializer_t, bytes>;
+template <typename T> using RPCReceiverBuilder = typename simpleRPC::RPCReceiverBuilder<T>;
+
 class Test {
 
   void foo() {}
@@ -10,7 +20,7 @@ class Test {
   int bop(int i, int d) { return i + d; }
 
   void register_rpc_targets(RPCReceiverBuilder<Test> &r) {
-    r.build_receiver(name_fun<0>(&Test::foo), name_fun<1>(&Test::bar), name_fun<2>(&Test::baz),
+    r.build_receiver(this, name_fun<0>(&Test::foo), name_fun<1>(&Test::bar), name_fun<2>(&Test::baz),
                      name_fun<3>(&Test::bop));
   }
 
