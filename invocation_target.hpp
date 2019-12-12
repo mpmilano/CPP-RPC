f211b5ca286e1bf11544507867c4e78f760c4153
Matthew Milano <milano@cs.cornell.edu>
2019-12-12 16:41:24 -0500

    initial construction appears to work

--- invocation_target.hpp
+++ invocation_target.hpp
@@ -7,30 +7,44 @@
 #define TOF(arg) std::decay_t<decltype(arg)>
 
 template <typename Deserializer, typename Serializer, typename Bytes> struct RPCFramework {
-  struct GenericSingleRPCReceiver {
-    virtual Bytes invoke(const Deserializer &, const Serializer &, Bytes &) = 0;
+  template <typename Class> struct GenericSingleRPCReceiver {
+    virtual Bytes invoke(Class &, const Deserializer &, const Serializer &, Bytes &) = 0;
     virtual ~GenericSingleRPCReceiver() {}
   };
 
   template <typename> struct SingleRPCReceiver;
-  template <int id, typename MemberFun>
-  struct SingleRPCReceiver<named_member_fun<id, MemberFun>> : public GenericSingleRPCReceiver {
-    SingleRPCReceiver(MemberFun) {}
-    Bytes invoke(const Deserializer &, const Serializer &, Bytes &) override { return nullptr; }
+
+  template <int id, typename Class, typename Ret, typename... Args>
+  struct SingleRPCReceiver<named_member_fun<id, Ret (Class::*)(Args...)>> : public GenericSingleRPCReceiver<Class> {
+    Ret (Class::*method)(Args...);
+    SingleRPCReceiver(Ret (Class::*method)(Args...)) : method(method) {}
+    Ret invoke_step1(Class &_this, const Deserializer &d, Bytes &b) const {
+      return std::apply([&](Args *... a) { return (_this.*method)(*a...); },
+                        d.template deserialize<Args...>(std::move(b)));
+    }
+
+    Bytes invoke(Class &_this, const Deserializer &d, const Serializer &s, Bytes &b) override {
+      if constexpr (!std::is_void<Ret>::value)
+        return s.serialize(invoke_step1(_this, d, b));
+      else {
+        invoke_step1(_this, d, b);
+        return Bytes{};
+      }
+    }
   };
 
   template <typename Class, typename... NamedMethods> class RPCReceiver {
     Class *_this;
 
-    std::array<std::unique_ptr<GenericSingleRPCReceiver>, sizeof...(NamedMethods)> invocation_targets;
+    std::array<std::unique_ptr<GenericSingleRPCReceiver<Class>>, sizeof...(NamedMethods)> invocation_targets;
 
   public:
     RPCReceiver(Class *c, typename NamedMethods::fun_t... f)
         : _this(c), invocation_targets{
-                        std::unique_ptr<GenericSingleRPCReceiver>{new SingleRPCReceiver<NamedMethods>(f)}...} {}
+                        std::unique_ptr<GenericSingleRPCReceiver<Class>>{new SingleRPCReceiver<NamedMethods>(f)}...} {}
 
     Bytes invoke(const Deserializer &deserializer, const Serializer &serializer, Bytes &serialized, int target) {
-      return invocation_targets[target]->invoke(deserializer, serializer, serialized);
+      return invocation_targets[target]->invoke(*_this, deserializer, serializer, serialized);
     }
   };
 
--- main.cpp
+++ main.cpp
@@ -1,19 +1,58 @@
 #include "invocation_target.hpp"
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <type_traits>
+#include <vector>
 
+template <int i, typename other_tuple, typename T1, typename... T2>
+void exec_all(std::tuple<T1, T2...> &out, const other_tuple &in) {
+  std::get<i>(out) = std::get<i>(in)();
+  if constexpr (sizeof...(T2) > i)
+    exec_all<i + 1>(out, in);
+}
+
+template <int> void exec_all(const std::tuple<> &, const std::tuple<> &) {}
+
+template <typename tuple1, typename tuple2> void exec_all(tuple1 &out, const tuple2 &in) { exec_all<0>(out, in); }
+
+using Bytes = std::unique_ptr<std::vector<char>>;
 struct deserializer_t {
-  template <typename T> static T *deserialize(char *) { return nullptr; }
+  template <typename... T>
+  static std::enable_if_t<(std::is_pod<T>::value && ... && true), std::tuple<T *...>> deserialize(Bytes b) {
+    auto buf_ptr = b->data();
+    std::tuple<T *...> out;
+    exec_all<std::tuple<T *...>>(out, std::make_tuple([&]() -> T * {
+                                   auto ret = ((T *)buf_ptr);
+                                   buf_ptr += sizeof(T);
+                                   return ret;
+                                 }...));
+    return out;
+  }
 };
 struct serializer_t {
-  static char *serialize(...) { return nullptr; }
+  template <typename... T>
+  static std::enable_if_t<(std::is_pod<T>::value && ... && true), Bytes> serialize(const T &... t) {
+    std::vector<char> *v = new std::vector<char>{};
+    v->resize((sizeof(t) + ... + 0));
+    auto insert_ptr = v->data();
+
+    std::vector<std::function<void()>> funcs{[&] {
+      std::memcpy(insert_ptr, &t, sizeof(t));
+      insert_ptr += sizeof(t);
+    }...};
+    for (auto f : funcs)
+      f();
+    return Bytes{v};
+  }
 };
-using Bytes = char *;
 using simpleRPC = RPCFramework<deserializer_t, serializer_t, Bytes>;
 template <typename T> using RPCReceiverBuilder = typename simpleRPC::RPCReceiverBuilder<T>;
 
 struct Test {
 
   void foo() {}
-  void bar(int, double) {}
+  void bar(int a, double b) { std::cout << "bar called with " << a << " and " << b << std::endl; }
 
   int baz() { return 0; }
 
@@ -21,7 +60,7 @@ struct Test {
 
   auto register_rpc_targets(RPCReceiverBuilder<Test> &r) {
     return r.build_receiver(this, name_fun<0>(&Test::foo), name_fun<1>(&Test::bar), name_fun<2>(&Test::baz),
-                     name_fun<3>(&Test::bop));
+                            name_fun<3>(&Test::bop));
   }
 
   void register_rpc_invocations() {}
@@ -31,9 +70,6 @@ int main() {
   Test t;
   RPCReceiverBuilder<Test> r;
   auto rpc = t.register_rpc_targets(r);
-  char bytes[1024] = {0};
-  using charp = char*;
-  charp ptr1 = bytes;
-  charp& ptr2 = ptr1;
-  rpc.invoke(deserializer_t{},serializer_t{},ptr2,0);
+  auto vec = serializer_t{}.serialize((int)12, (double)23.5);
+  rpc.invoke(deserializer_t{}, serializer_t{}, vec, 1);
 }
